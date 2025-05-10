2d46b977dc045d9b8cd5cb90d03d49e2e5fbd9a1
MaKatre <martina.katreniakovahq@gmail.com>
2025-05-10 17:12:13 +0200

    snaha o include/require navbar

---
+++ pokus.php
@@ -0,0 +1,14 @@
+<?php
+    $title = 'POKUS';
+    $metaTags = 'tag1 tag2';
+    $currentPage = 'index';
+    require_once(__DIR__.'/header.php');
+    require_once(__DIR__.'parts')
+?>
+
+<?php
+    include "parts/header.php";
+    require "parts/navigation.php";
+    //echo "čo kurva toto je?";
+    include "parts/footer.php";
+?>
