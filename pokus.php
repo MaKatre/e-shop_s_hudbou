559f90e827d69e674dd3b3f4af37de6c3c5177f2
MaKatre <martina.katreniakovahq@gmail.com>
2025-05-12 22:09:56 +0200

    Commit

--- pokus.php
+++ pokus.php
@@ -1,14 +1,9 @@
 <?php
-    /*$title = 'POKUS';
-    $metaTags = 'tag1 tag2';
-    $currentPage = 'index';
-    require_once(__DIR__.'/header.php');
-    require_once(__DIR__.'parts')*/
+    
 ?>
 
 <?php
     include "parts/header.php";
-    require "parts/navigation.php";
-    //echo "čo kurva toto je?";
+    //require "parts/navigation.php";
     include "parts/footer.php";
 ?>
---
+++ sign_up.php
@@ -0,0 +1,12 @@
+<?php
+    include "parts/header.php";
+
+?>
+
+<body>
+    <h1>Sign In</h1>
+</body>
+
+<?php
+    include "parts/footer.php";
+?>
