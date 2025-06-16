c847958ee08455c483afb45362b7f94abed2473e
MaKatre <martina.katreniakovahq@gmail.com>
2025-06-16 16:31:06 +0200

    Commit

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
                             <a class="nav-link" href="index.php">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="about.php">About</a>
+                            <a class="nav-link " href="about.php">About</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="shop.php">Shop</a>
